c31ec9020c683471d3514b20c9e8699dd887bc48
Ethan Jensen <ethan.raymond.jensen@gmail.com>
2025-09-25 10:50:08 -0600

    completed stretch assignment for Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
@@ -10,13 +12,13 @@ class Program
         int keepRunning = 1;
         int sumOfNumberList = 0;
 
-        while (keepRunning !=  0) //ask for user input to input numbers until they enter 0
+        while (keepRunning != 0) //ask for user input to input numbers until they enter 0
         {
             Console.Write("Enter number: ");
             int numberToAdd = int.Parse(Console.ReadLine());
-            keepRunning =  numberToAdd;
+            keepRunning = numberToAdd;
             if (numberToAdd != 0) //only add the number if it isn't 0 to avoid adding 0 and messing up the average
-            { 
+            {
                 numberList.Add(numberToAdd);
             }
         }
@@ -30,9 +32,27 @@ class Program
         Console.WriteLine($"The sum is: {sumOfNumberList}");
 
         float averageOfNumberList = (float)sumOfNumberList / numberList.Count; //compute the average of the numbers in the list
-        Console.WriteLine($"The average is: {averageOfNumberList}"); 
+        Console.WriteLine($"The average is: {averageOfNumberList}");
 
         int maxNumber = numberList.Max(); //find the maximum/largest number in the list
         Console.WriteLine($"The largest number is: {maxNumber}");
+
+        int minPositiveNumber = maxNumber;
+        foreach (int number in numberList)
+        {
+            if (number > 0 && number < minPositiveNumber)
+            {
+                minPositiveNumber = number;
+            }
+        }
+        Console.WriteLine($"The smallest positive number is: {minPositiveNumber}");
+
+        Console.WriteLine("The sorted list is: ");
+        //sort list by numerical value
+        List<int> sortedNumberList = numberList.OrderBy(number => number).ToList();
+        foreach (int number in sortedNumberList)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
