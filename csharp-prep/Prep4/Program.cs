dd1c7b3be69d7d32617ed3f1a570aebbd4c253aa
Ethan Jensen <ethan.raymond.jensen@gmail.com>
2025-09-24 22:41:29 -0600

    added comments

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -15,7 +15,7 @@ class Program
             Console.Write("Enter number: ");
             int numberToAdd = int.Parse(Console.ReadLine());
             keepRunning =  numberToAdd;
-            if (numberToAdd != 0)
+            if (numberToAdd != 0) //only add the number if it isn't 0 to avoid adding 0 and messing up the average
             { 
                 numberList.Add(numberToAdd);
             }
