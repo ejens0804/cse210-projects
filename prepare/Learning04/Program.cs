24aee0b81072a048f6af1fdac7a66e2b0e9b66fc
ejens0804 <ethan.raymond.jensen@gmail.com>
2025-10-29 00:04:40 -0600

    completed the Learning04 Inheritance activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment math1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        math1.GetSummary();
+        math1.GetHomeworkList();
+
+        WritingAssignment wa = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        wa.GetSummary();
+        wa.GetWritingInformation();
     }
 }
