e381dbd9457e3eae8bd305d5aec2cc783775632b
ejens0804 <ethan.raymond.jensen@gmail.com>
2025-11-22 23:43:48 -0700

    added submission comment

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,3 +1,16 @@
+/*
+In this program I fulfilled all of the Core Requirements. I have exceeded the requirements
+and shown creativity by adding 3 more classes that add additional functionality. 
+The user is able to reset the program, and there is a money system involved with the goals
+so that as the user completes goals they are awarded with XP and Moolah (the program currency).
+The XP determines what their level name is called (the more xp they have, the cooler the name)
+and they can use the moolah in the rewards shop to buy a reward. 
+*/
+
+
+
+
+
 using System;
 using System.Security.Cryptography;
 using System.Text.Json; // modern JSON serializer
