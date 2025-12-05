ead045f62ada7e99b4c7d80ba48f2c17dc9bf095
ejens0804 <ethan.raymond.jensen@gmail.com>
2025-12-04 23:51:35 -0700

    finished writing the Business Account class

--- final/FinalProject/BusinessAccount.cs
+++ final/FinalProject/BusinessAccount.cs
@@ -1,42 +1,90 @@
-public class BusinessAccount : BaseAccount
+using System;
+
+namespace FinalProject
 {
-    // Attributes/Member Variables
-    private string _businessName;
-    private decimal _transactionFeePerOperation;
-    private int _freeTransactionLimit;
-    private int _currentTransactionCount;
-
-
-    // Constructor
-    public BusinessAccount()
+    public class BusinessAccount : BaseAccount
     {
-        
-    }
+        // Attributes/Member Variables
+        private string _businessName;
+        private decimal _transactionFeePerOperation;
+        private int _freeTransactionLimit;
+        private int _currentTransactionCount;
 
 
-    // Methods
-    public override void Withdraw(decimal amount)
-    {
-        
-    }
+        // Constructor
+        public BusinessAccount()
+        {
+            _businessName = "";
+            _transactionFeePerOperation = 0.50m;
+            _freeTransactionLimit = 100;
+            _currentTransactionCount = 0;
+            _interestRate = 1.5m;
+            _minimumBalance = 500.00m;
+        }
 
-    public override void Deposit(decimal amount)
-    {
-        
-    }
 
-    public override decimal CalculateInterest()
-    {
-        return 0m;
-    }
+        // Methods
+        public override void Deposit(decimal amount)
+        {
+            base.Deposit(amount);
+            _currentTransactionCount++;
 
-    public void ResetMonthlyTransactionCount()
-    {
-        
-    }
+            // Charge fee if over the limit
+            if (_currentTransactionCount > _freeTransactionLimit)
+            {
+                decimal fee = CalculateTransactionFees();
+                _balance -= fee;
+                Console.WriteLine($"Transaction fee of ${fee:N2} charged");
+            }
+        }
 
-    public decimal CalculateTransactionFees()
-    {
-        return 0m;
+        public override bool Withdraw(decimal amount)
+        {
+            bool success = base.Withdraw(amount);
+
+            if (success)
+            {
+                _currentTransactionCount++;
+
+                // Charge fee if over the limit
+                if (_currentTransactionCount > _freeTransactionLimit)
+                {
+                    decimal fee = CalculateTransactionFees();
+
+                    _balance -= fee;
+                    Console.WriteLine($"Transaction fee of ${fee:N2} charged");
+                }
+            }
+
+            return success;
+        }
+
+        public override decimal CalculateInterest()
+        {
+            return _balance * (_interestRate / 100) / 12;
+        }
+
+        public void ResetMonthlyTransactionCount()
+        {
+            _currentTransactionCount = 0;
+        }
+
+        public decimal CalculateTransactionFees()
+        {
+            if (_currentTransactionCount > _freeTransactionLimit)
+            {
+                return _transactionFeePerOperation;
+            }
+            return 0m;
+        }
+
+        public override void DisplayAccountInfo()
+        {
+            base.DisplayAccountInfo();
+            Console.WriteLine($"Business Name: {_businessName}");
+            Console.WriteLine($"Transactions This Month: {_currentTransactionCount}");
+            Console.WriteLine($"Free Transaction Limit: {_freeTransactionLimit}");
+            Console.WriteLine($"Transaction Fee: ${_transactionFeePerOperation:N2}");
+        }
     }
 }
